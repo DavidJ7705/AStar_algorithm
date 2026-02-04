abd11d0294eabc1260133eb5305c6e93263e564c
DAVID JAYAKUMAR - STUDENT <g00419108@atu.ie>
2026-02-04 14:34:56 +0000

    Fixed up some formating of the output, also changed the obstacle logic

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -17,14 +17,18 @@ PathFind::PathFind(int row, int col) : numRows(row), numCols(col) { // construct
 void PathFind::printGrid() {	//function to print the grid
 	for (auto& row : grid) { //for range loop to print each element in the grid
 		for (auto& element : row) {
-			std::cout << element << " ";
+			if(element == 0)
+				std::cout << ". "; //printing . for empty cell
+			else {
+				std::cout << "# "; //printing # for obstacle
+			}
 		}
 		std::cout << std::endl;
 	}
 }
 
 void PathFind::setObstacle(int row, int col) {	//function to set obstacles in the grid
-	if (row >= 0 && row <= numRows && col >= 0 && col <= numCols) { //checking if the row and column are within bounds
+	if (row >= 0 && row < numRows && col >= 0 && col < numCols) { //checking if the row and column are within bounds
 		grid[row][col] = 1; //setting the obstacle at specified position
 	}
 	else {
